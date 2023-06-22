Fix jacobian call

tests/test_grad/test_hessian.py
@@ -21,26 +21,16 @@ import pytest
 import torch
 
 from tad_dftd3 import dftd3, util
-from tad_dftd3.__version__ import __torch_version__
 from tad_dftd3.typing import Tensor
 
 from ..samples import samples
 from ..utils import reshape_fortran
-
-if __torch_version__ < (2, 0, 0):
-    try:
-        from functorch import jacrev  # type: ignore
-    except ModuleNotFoundError:
-        jacrev = None
-else:
-    from torch.func import jacrev  # type: ignore
 
 sample_list = ["LiH", "SiH4", "PbH4-BiH3", "MB16_43_01"]
 
 tol = 1e-8
 
 
-@pytest.mark.skipif(jacrev is None, reason="Hessian tests require functorch")
 def test_fail() -> None:
     sample = samples["LiH"]
     numbers = sample["numbers"]
@@ -52,7 +42,6 @@ def test_fail() -> None:
         util.hessian(dftd3, (numbers, positions, param), argnums=2)
 
 
-@pytest.mark.skipif(jacrev is None, reason="Hessian tests require functorch")
 def test_zeros() -> None:
     d = torch.randn(2, 3, requires_grad=True)
 
@@ -63,7 +52,6 @@ def test_zeros() -> None:
     assert pytest.approx(torch.zeros([*d.shape, *d.shape])) == hess.detach()
 
 
-@pytest.mark.skipif(jacrev is None, reason="Hessian tests require functorch")
 @pytest.mark.parametrize("dtype", [torch.double])
 @pytest.mark.parametrize("name", sample_list)
 def test_single(dtype: torch.dtype, name: str) -> None:
@@ -82,7 +70,7 @@ def test_single(dtype: torch.dtype, name: str) -> None:
 
     ref = reshape_fortran(
         sample["hessian"].type(dtype),
-        torch.Size((numbers.shape[0], 3, numbers.shape[0], 3)),
+        torch.Size(2 * (numbers.shape[-1], 3)),
     )
 
     # variable to be differentiated
@@ -95,7 +83,6 @@ def test_single(dtype: torch.dtype, name: str) -> None:
 
 
 # TODO: Figure out batched Hessian computation
-@pytest.mark.skipif(jacrev is None, reason="Hessian tests require functorch")
 @pytest.mark.parametrize("dtype", [torch.double])
 @pytest.mark.parametrize("name1", ["LiH"])
 @pytest.mark.parametrize("name2", sample_list)
@@ -127,15 +114,11 @@ def skip_test_batch(dtype: torch.dtype, name1: str, name2: str) -> None:
         [
             reshape_fortran(
                 sample1["hessian"].type(dtype),
-                torch.Size(
-                    (sample1["numbers"].shape[0], 3, sample1["numbers"].shape[0], 3)
-                ),
+                torch.Size(2 * (sample1["numbers"].shape[-1], 3)),
             ),
             reshape_fortran(
                 sample2["hessian"].type(dtype),
-                torch.Size(
-                    (sample2["numbers"].shape[0], 3, sample2["numbers"].shape[0], 3)
-                ),
+                torch.Size(2 * (sample2["numbers"].shape[-1], 3)),
             ),
         ]
     )