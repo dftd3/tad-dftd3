086cd4770b8e7a04798f4a2fbe1b7e48f8805890
FreeSoulIG <igorgordiy2000@gmail.com>
2023-10-20 13:35:09 +0300

    slightly modififed the dispersion test for AmF3.

--- tests/test_disp.py
+++ tests/test_disp.py
@@ -157,39 +157,34 @@ def test_atm_single(dtype: torch.dtype, name: str) -> None:
 
 
 @pytest.mark.parametrize("dtype", [torch.float, torch.double])
-@pytest.mark.parametrize("name1", ["SiH4", "MB16_43_01"])
+@pytest.mark.parametrize("name1", ["SiH4", "MB16_43_01", "AmF3"])
 @pytest.mark.parametrize("name2", ["SiH4"])
-@pytest.mark.parametrize("name3", ["AmF3"])
-def test_atm_batch(dtype: torch.dtype, name1: str, name2: str, name3: str) -> None:
+def test_atm_batch(dtype: torch.dtype, name1: str, name2: str) -> None:
     tol = sqrt(torch.finfo(dtype).eps) * 10
 
-    sample1, sample2, sample3 = samples[name1], samples[name2], samples[name3]
+    sample1, sample2 = samples[name1], samples[name2]
     numbers = util.pack(
         [
             sample1["numbers"],
             sample2["numbers"],
-            sample3["numbers"],
         ]
     )
     positions = util.pack(
         [
             sample1["positions"].type(dtype),
             sample2["positions"].type(dtype),
-            sample3["positions"].type(dtype),
         ]
     )
     c6 = util.pack(
         [
             sample1["c6"].type(dtype),
             sample2["c6"].type(dtype),
-            sample3["c6"].type(dtype),
         ]
     )
     ref = util.pack(
         [
             (sample1["disp3"] - sample1["disp2"]).type(dtype),
             (sample2["disp3"] - sample2["disp2"]).type(dtype),
-            (sample3["disp3"] - sample3["disp2"]).type(dtype),
         ]
     )
 
