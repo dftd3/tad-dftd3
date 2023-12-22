f1167159275ae336bce4f3a480e43abb36206ce9
Marvin Friede <51965259+marvinfriede@users.noreply.github.com>
2023-12-22 11:09:07 +0100

    Store radii (#42)

--- src/tad_dftd3/data/r4r2.py
+++ src/tad_dftd3/data/r4r2.py
@@ -18,8 +18,8 @@ Data: Expectation values
 
 PBE0/def2-QZVP atomic values calculated by S. Grimme in Gaussian (2010),
 rare gases recalculated by J. Mewes with PBE0/aug-cc-pVQZ in Dirac (2018).
-Also new super heavies Cn,Nh,Fl,Lv,Og and Am-Rg calculated at 4c-PBE/Dyall-AE4Z
-(Dirac 2022)
+Also new super heavies Cn, Nh, Fl, Lv, Og and Am-Rg calculated at
+4c-PBE/Dyall-AE4Z (Dirac 2022).
 """
 import torch
 
